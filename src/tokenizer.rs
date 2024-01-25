0974b918d0e00009add31c1c48f5aa510ca2c989
Tim Barnes <tw@timandmarina.com>
2024-01-24 21:02:46 -0800

    Tokenizer first version

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -2,42 +2,142 @@
 // Return one space-delimited token at a time.
 // Cache the remainder of the line.
 
+use std::collections::VecDeque;
 use std::fs::File;
-use std::io::{self};
+use std::io::{self, BufRead, BufReader};
 
-struct Tokenizer {
-    filename: &str,    // name of the file to read from, or "stdin"
-    stream: File,      // file handle
-    line: &str,        // the text of the current line
-    tokens: Vec<&str>, // a vector of tokens, successively popped off until empty and a new line is read.
+enum TokenSource {
+    Stdin,
+    Stream(BufReader<File>),
+}
+pub struct Tokenizer {
+    source: TokenSource,
+    line: String,             // the text of the current line
+    tokens: VecDeque<String>, // a vector of tokens, successively popped off until empty and a new line is read.
+    success: bool, // Set to true if the Tokenizer was properly created. Fails on file errors
 }
 
-impl Tokenizer {
-    pub fn new(path: &str) -> Tokenizer {
-        // Initialize a tokenizer. path is a filename or stdin
-        Tokenizer {
-            filename: if path == "stdin" { "" } else { path },
-            stream: if path == "stdin" {
-                io::stdin
-            } else {
-                File.open(path)
-            },
-            line: "",
-            tokens: Vec::new(),
+pub impl Tokenizer {
+    pub fn new(file_path: Option<&str>) -> Tokenizer {
+        // Initialize a tokenizer.
+        match file_path {
+            None => {
+                return Tokenizer {
+                    source: TokenSource::Stdin,
+                    line: String::new(),
+                    tokens: VecDeque::new(),
+                    success: true,
+                }
+            }
+            Some(filepath) => {
+                let file = File::open(filepath);
+                match file {
+                    Ok(file) => {
+                        return Tokenizer {
+                            source: TokenSource::Stream(BufReader::new(file)),
+                            line: String::new(),
+                            tokens: VecDeque::new(),
+                            success: true,
+                        }
+                    }
+                    Err(_) => {
+                        println!("File not able to be opened: {:?}", file_path);
+                        return Tokenizer {
+                            source: TokenSource::Stdin,
+                            line: String::new(),
+                            tokens: VecDeque::new(),
+                            success: false,
+                        };
+                    }
+                }
+            }
         }
     }
 
-    fn read_line(&self) -> Option(&str) {
-        // Read a line, returning it if there is one
+    pub fn is_valid(&self) -> bool {
+        self.success
+    }
+
+    fn get_line(&mut self) -> bool {
+        // Read a line, storing it if there is one
         // In interactive (stdin) mode, blocks until the user provides a line.
-        if let line = self.stream.read_line() {
-            Some(line)
-        } else {
-            None
+        // Returns true if a line was read, false if not.
+        match self.source {
+            TokenSource::Stdin => {
+                // Read from stdin
+                self.line.clear();
+                if let Err(_) = io::stdin().read_line(&mut self.line) {
+                    return false;
+                } else {
+                    println!("Got some values: {:?}", self.line);
+                    self.tokenize();
+                    println!("tokens are: {:?}", self.tokens);
+                    return true;
+                }
+            }
+            TokenSource::Stream(ref mut file) => {
+                // Read from a file. TokenSource is a BufReader
+                println!("get_line reading from file");
+                if let Err(_) = &file.read_line(&mut self.line) {
+                    return false;
+                } else {
+                    println!("self.line: {:?}", self.line);
+                    return true;
+                }
+            }
         }
     }
 
-    pub fn get_token(&mut self) -> Option(&str) {
-        // Returns the next token from the stream, if there is one
+    fn tokenize(&mut self) -> bool {
+        // Tokenize the current line, returning true if successful
+        // Only fails if the line is empty
+        //
+        println!("tokenize");
+        if self.line.is_empty() {
+            return false;
+        } else {
+            let mut inside_quotes = false;
+            let mut current_word = String::new();
+            for c in self.line.chars() {
+                match c {
+                    ' ' | '\n' if !inside_quotes => {
+                        if !current_word.is_empty() {
+                            self.tokens.push_back(current_word.clone());
+                            current_word.clear();
+                        }
+                    }
+                    '\"' => inside_quotes = !inside_quotes,
+                    _ => current_word.push(c),
+                }
+            }
+            if !current_word.is_empty() {
+                self.tokens.push_back(current_word);
+            }
+        }
+        self.tokens.len() > 0
+    }
+
+    pub fn get_token(&mut self) -> Option<String> {
+        // Returns the next token from the stream, if there is one, otherwise None
+        println!("get_token");
+        if self.tokens.is_empty() {
+            println!("no more tokens; need a new line");
+            if self.get_line() {
+                let t = self.tokens.pop_front();
+                match t {
+                    Some(t) => {
+                        return Some(t);
+                    }
+                    _ => {
+                        return None;
+                    }
+                }
+            } else {
+                println!("unable to get new line");
+                return None;
+            }
+        } else {
+            return self.tokens.pop_front();
+        }
     }
 }
